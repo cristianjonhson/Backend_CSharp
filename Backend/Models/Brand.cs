37bbc1864f259a4d02f8935b6ed2569315cc0891
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-13 20:37:57 -0300

    Creacion Modelo Marca

---
+++ Backend/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 BrandId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+         
+    }
+}
