d705d941ca8349ce9db0b9c456c2e02aca0a2b86
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-13 20:08:14 -0300

    Explicando con comentarios

--- Backend/Services/IPostsService.cs
+++ Backend/Services/IPostsService.cs
@@ -4,8 +4,11 @@ using System.Threading.Tasks;
 
 namespace Backend.Services
 {
+    // Definición de la interfaz IPostsService
     public interface IPostsService
     {
+        // Declaración del método GetPosts
+        // Este método devuelve una tarea que representa una colección de objetos PostDto
         Task<IEnumerable<PostDto>> GetPosts();
     }
 }
--- Backend/Services/PostsService.cs
+++ Backend/Services/PostsService.cs
@@ -1,34 +1,41 @@
-﻿using Backend.DTOs;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using Backend.DTOs;  // Importa el espacio de nombres que contiene la clase PostDto
+using System.Text.Json;  // Importa el espacio de nombres que contiene JsonSerializer
+
 
 namespace Backend.Services
 {
+    // Implementación de la interfaz IPostsService
     public class PostsService : IPostsService
     {
-        private HttpClient _httpClient;
+        private HttpClient _httpClient;  // Campo para almacenar una instancia de HttpClient
 
+        // Constructor de la clase
         public PostsService()
         {
-            _httpClient = new HttpClient();
+            _httpClient = new HttpClient();  // Crea una nueva instancia de HttpClient
         }
 
+        // Implementación del método de la interfaz IPostsService
         public async Task<IEnumerable<PostDto>> GetPosts()
         {
-            string url = "https://jsonplaceholder.typicode.com/posts";
+            string url = "https://jsonplaceholder.typicode.com/posts";  // URL de la fuente de datos remota
+
+            // Realiza una solicitud GET asincrónica a la URL especificada y espera la respuesta
             var result = await _httpClient.GetAsync(url);
+
+            // Lee el contenido de la respuesta como una cadena de manera asincrónica
             var body = await result.Content.ReadAsStringAsync();
 
+            // Opciones de configuración para el deserializador JSON
             var options = new JsonSerializerOptions
             {
-                PropertyNameCaseInsensitive = true,
+                PropertyNameCaseInsensitive = true,  // Ignora la distinción entre mayúsculas y minúsculas al deserializar
             };
 
+            // Deserializa la cadena JSON en una colección de objetos PostDto
             var posts = JsonSerializer.Deserialize<IEnumerable<PostDto>>(body, options);
 
-            return posts;
+            return posts;  // Devuelve la colección de objetos PostDto
         }
     }
 }
