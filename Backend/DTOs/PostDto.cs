101bb7d60e212995a3cc6a91d4df0aaeb8baf83f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-13 12:53:57 -0300

    Creacion de PostDto

---
+++ Backend/DTOs/PostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.DTOs
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public string? Description { get; set; }
+
+        public string? Body { get; set; }
+    }
+}
