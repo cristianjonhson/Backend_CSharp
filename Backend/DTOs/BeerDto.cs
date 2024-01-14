131a51013c704dea0632e297beb338daf337b9bf
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-13 23:38:02 -0300

    Creacion Dtos beer para crud

---
+++ Backend/DTOs/BeerDto.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.DTOs
+{
+    public class BeerDto
+    {
+        public int BeerId { get; set; }
+        public string BeerName { get; set; }
+
+        public string BeerDescription { get; set; }
+
+        public string BeerType { get; set; }
+
+        public decimal Alcohol { get; set; }
+        public int BrandId { get; set; }
+    }
+}
