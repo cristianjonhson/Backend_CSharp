3ecb2d91f4d991f22477fa9a5b8652b4c9d094ba
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-17 12:25:11 -0300

    Mensaje de error personalizados

--- Backend/Validators/BeerInsertValidator.cs
+++ Backend/Validators/BeerInsertValidator.cs
@@ -9,7 +9,20 @@ public class BeerInsertValidator : AbstractValidator<BeerInsertDto>
     public BeerInsertValidator()
     {
         // Define una regla de validación para la propiedad BeerName del DTO BeerInsertDto
-        // Asegura que BeerName no esté vacío
-        RuleFor(x => x.BeerName).NotEmpty();
+        // Asegura que BeerName no esté vacío y proporciona un mensaje personalizado si la regla no se cumple
+        RuleFor(x => x.BeerName).NotEmpty().WithMessage("El nombre es obligatorio");
+
+        // Define una regla de validación para la propiedad BeerName del DTO BeerInsertDto
+        // Asegura que BeerName tenga una longitud entre 2 y 20 caracteres y proporciona un mensaje personalizado si la regla no se cumple
+        RuleFor(x => x.BeerName).Length(2, 20).WithMessage("El nombre debe tener de 2 a 20 caracteres");
+
+        // Define una regla de validación para la propiedad BrandId del DTO BeerInsertDto
+        // Asegura que BrandId no sea nulo, sea mayor que 0 y proporciona un mensaje personalizado si la regla no se cumple
+        RuleFor(x => x.BrandId).NotNull().WithMessage(x => "La marca es obligatoria");
+        RuleFor(x => x.BrandId).GreaterThan(0).WithMessage(x => "Error con el valor enviado de marca");
+
+        // Define una regla de validación para la propiedad Alcohol del DTO BeerInsertDto
+        // Asegura que Alcohol sea mayor que 0 y proporciona un mensaje personalizado si la regla no se cumple
+        RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage(x => "El {PropertyName} debe ser mayor a 0 ");
     }
 }
