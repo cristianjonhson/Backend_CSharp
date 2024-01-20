dc754a8aa055fd6f9beff499fb15a0419f8bbdcb
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-20 19:46:05 -0300

    Definicion de interface para capa Service Brand

---
+++ Backend/Services/IBrandService.cs
@@ -0,0 +1,15 @@
+﻿using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Services
+{
+    public interface IBrandService
+    {
+        Task<IEnumerable<BrandDto>> GetAllBrands();
+        Task<BrandDto> GetBrandById(long id);
+        Task<BrandDto> AddBrand(BrandInsertDto brandInsertDto);
+        Task<IActionResult> UpdateBrand(long id, BrandUpdateDto brandUpdateDto);
+
+    }
+}
