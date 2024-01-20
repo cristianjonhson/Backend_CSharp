0ebbcc09ce5115884b3c47a731287a4f2dd8e895
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-20 18:36:55 -0300

    Creacion del DTO de la Brand

---
+++ Backend/DTOs/BrandDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.DTOs
+{
+    // DTO para transferir datos de la entidad Brand
+    public class BrandDto
+    {
+        // Propiedad para el identificador único de la marca
+        public Int64 BrandId { get; set; }
+
+        // Propiedad para el nombre de la marca
+        public string? Name { get; set; }
+
+        // Propiedad para la descripción de la marca
+        public string? Description { get; set; }
+    }
+}
+
