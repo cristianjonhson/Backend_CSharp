e90228e09b325ccc87b7d1f7e01ee527fcb8dada
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-21 00:02:49 -0300

    Definicion interface para capa Repository Brand

---
+++ Backend/Repository/IBrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Backend.Repository
+{
+    public interface IBrandRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllBrands();
+
+        Task<T?> GetBrandById(int id);
+
+        Task<T> Add(T entity);
+
+        Task<IActionResult> Update(int id, T entity);
+
+        Task Save();
+    }
+}
