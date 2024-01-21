17120a8aa38cd54109cc1787cae7ceddf15ebe3c
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-20 23:17:39 -0300

    Creacion de Repository Beer

---
+++ Backend/Repository/BeerRepository.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Repository
+{
+    public class BeerRepository : IRepository<Beer>
+    {
+        public Task<Beer> Add(Beer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Beer>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Beer> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Update(int id, Beer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
