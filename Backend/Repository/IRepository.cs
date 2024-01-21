71187a545f7ab5ee9799a3074f4728acbb523e7d
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-20 23:13:00 -0300

    Definicion de Interface para capa Repository

---
+++ Backend/Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Repository
+{
+    public interface IRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        // Obtener un elemento por su ID.
+        Task<TEntity> GetById(int id);
+
+        // Agregar un nuevo elemento a la entidad.
+        Task<TEntity> Add(TEntity entity);
+
+        // Actualizar un elemento de la entidad por su ID.
+        Task<IActionResult> Update(int id, TEntity entity);
+
+        Task Save();
+    }
+
+
+}
+}
