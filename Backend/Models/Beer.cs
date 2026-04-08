616e0dee5647139c23810159b0073b503134047b
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-08 14:03:42 -0400

    feat(model): agrega BrandId en entidad Beer para mapear la FK

--- Backend/Models/Beer.cs
+++ Backend/Models/Beer.cs
@@ -9,17 +9,19 @@ namespace Backend.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BeerId { get; set; }
 
-        public string? BeerName { get; set;}
-        
-        public string? BeerDescription { get; set;}
+        public string? BeerName { get; set; }
 
-        public string? BeerType { get; set;}
-        
-        [Column(TypeName ="decimal(18,2)")]
+        public string? BeerDescription { get; set; }
+
+        public string? BeerType { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Alcohol { get; set; }
+
+        public long BrandId { get; set; }
 
         [ForeignKey("BrandId")]
         public virtual Brand? Brand { get; set; }
-    
+
     }
 }
